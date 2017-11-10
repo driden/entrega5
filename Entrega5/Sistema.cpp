082f34099d3429950efae634317edd59c01764ad
driden <driden_87@hotmail.com>
2017-11-10 17:52:06 -0300

    skylines

--- Entrega5/Sistema.cpp
+++ Entrega5/Sistema.cpp
@@ -3,6 +3,7 @@
 #include "ListaOrd.h"
 #include "ListaEncadenadaImp.h"
 #include "ComparacionITira.h"
+#include "ListaEncadenada.h"
 
 Sistema::Sistema()
 {
@@ -17,45 +18,156 @@ void Sistema::TransponerMatriz(Matriz<nat> matriz)
 	return; //retorno por defecto
 };
 
-Puntero<ListaOrd<Puntero<ITira>>> GetSiluetas(Array<Puntero<IEdificio>> ciudad)
+Array<Puntero<ListaOrd<Puntero<ITira>>>> GetSiluetas(Array<Puntero<IEdificio>> ciudad)
 {
 	Puntero<Comparacion<Puntero<ITira>>> pComp = new ComparacionITira();
 	Comparador<Puntero<ITira>> compTira(pComp);
 
 	Puntero<ListaOrd<Puntero<ITira>>> siluetas = new ListaEncadenadaImp<Puntero<ITira>>(compTira);
+	
+	Array<Puntero<ListaOrd<Puntero<ITira>>>> arrSiluetas(ciudad.Largo);
+
 
 	for (nat i = 0; i < ciudad.Largo ; i++)
 	{
 		Puntero<IEdificio> edificio = ciudad[i];
+		Puntero<ListaOrd<Puntero<ITira>>> tiras = new ListaEncadenadaImp<Puntero<ITira>>(compTira);
+		arrSiluetas[i] = tiras;
+		
 		Puntero<ITira> tira;
 		
 		tira = new Tira(edificio->ObtenerXInicial(), edificio->ObtenerAltura());
-		siluetas->InsertarOrdenado(tira);
+		tiras->InsertarOrdenado(tira);
 		tira = new Tira(edificio->ObtenerXFinal(), 0);
-		siluetas->InsertarOrdenado(tira);
+		tiras->InsertarOrdenado(tira);
 	}
 
-	return siluetas;
+
+	// Ordenar el arreglo antes de devolverlo por la x del principio ?
+	return arrSiluetas;
 }
 
+int Max(int n1, int n2)
+{
+	return n1 > n2 ? n1 : n2;
+}
 
+Puntero<Lista<Puntero<ITira>>> MergeSkyLines(Puntero<Lista<Puntero<ITira>>> skIzq, Puntero<Lista<Puntero<ITira>>> skDer)
+{
+	//assert(estaOrdenada(skIzq));
+	//assert(estaOrdenada(skDer));
+	//assert(noTieneAlturasRepetidas(skIzq));
+	//assert(noTieneAlturasRepetidas(skDer));
+
+	
+
+	Iterador<Puntero<ITira>> itIzq = skIzq->ObtenerIterador(), itDer = skDer->ObtenerIterador();
+
+
+
+	nat hIzq = 0, hDer = 0;
+	Puntero<ITira> tira;
+
+	Puntero<Comparacion<Puntero<ITira>>> pComp = new ComparacionITira();
+	Comparador<Puntero<ITira>> compTira(pComp);
+	Puntero<Lista<Puntero<ITira>>> lMerge = new ListaEncadenada<Puntero<ITira>>(compTira);
+
+	while(itIzq.HayElemento() && itDer.HayElemento())
+	{
+		Puntero<ITira> pDer = itDer.ElementoActual(), pIzq = itIzq.ElementoActual();
+		
+		nat x = 0;
+
+		if (pDer->ObtenerX() < pIzq->ObtenerAltura())
+		{
+			hDer = pDer->ObtenerAltura();
+			x = pDer->ObtenerX();
+
+			itDer.Avanzar();
+		}
+		else
+		{
+			hIzq = pIzq->ObtenerAltura();
+			x = pIzq->ObtenerX();
+
+			itIzq.Avanzar();
+		}
+		nat nuevaH = Max(hIzq, hDer);
+		
+		//controlo que no haya un nodito con la misma altura ya
+		if (tira->ObtenerAltura() == nuevaH) continue;
+		
+		tira = new Tira(x, nuevaH);
+		lMerge->Insertar(tira);
+	}
+
+	if(itIzq.HayElemento())
+	{
+		while(itIzq.HayElemento())
+		{
+			lMerge->Insertar(itIzq.ElementoActual());
+			itIzq.Avanzar();
+		}
+	}
+	else if (itDer.HayElemento())
+	{
+		while (itDer.HayElemento())
+		{
+			lMerge->Insertar(itDer.ElementoActual());
+			itDer.Avanzar();
+		}
+	}
+	return lMerge;
+}
+
+Puntero<Lista<Puntero<ITira>>> SkyLine(Array<Puntero<ListaOrd<Puntero<ITira>>>> tiras, int desde, int hasta)
+{
+	Puntero<Lista<Puntero<ITira>>> lista = nullptr;
+	if (desde < hasta)
+	{
+		int medio = (desde + hasta) / 2;
+		Puntero<Lista<Puntero<ITira>>> skIzq = SkyLine(tiras, desde, medio);
+		Puntero<Lista<Puntero<ITira>>> skDer = SkyLine(tiras, medio+1, hasta);
+		lista = MergeSkyLines(skIzq, skDer);
+	}
+	return lista;
+}
 Array<Puntero<ITira>> Sistema::CalcularSiluetaDeLaCiudad(Array<Puntero<IEdificio>> ciudad)
 {
 	// Paso 1: Convierto todos los IEdificio en ITiras de (x, altura)
-	Puntero<ListaOrd<Puntero<ITira>>> siluetas = GetSiluetas(ciudad);
+	Array<Puntero<ListaOrd<Puntero<ITira>>>> siluetas = GetSiluetas(ciudad);
 
 	std::cout << "Tiras:\n";
 	
-	Iterador<Puntero<ITira>> iterTiras = siluetas->ObtenerIterador();
+	for (nat i = 0; i < siluetas.Largo; i++)
+	{
+		Iterador<Puntero<ITira>> iterTiras = siluetas[i]->ObtenerIterador();
+		while (iterTiras.HayElemento())
+		{
+			Puntero<ITira> tiraActual = iterTiras.ElementoActual();
+			std::cout << "(x:" << tiraActual->ObtenerX() << ", h:" << tiraActual->ObtenerAltura() << ") ";
 
+			iterTiras.Avanzar();
+		}
+		std::cout << endl;
+	}
+	
+	std::cout << endl;
+
+	Puntero<Lista<Puntero<ITira>>> lsita = SkyLine(siluetas, 0, siluetas.Largo - 1);
+	Iterador<Puntero<ITira>> iterTiras = lsita->ObtenerIterador();
+	std::cout << " skyline: "<< endl;
 	while (iterTiras.HayElemento())
 	{
 		Puntero<ITira> tiraActual = iterTiras.ElementoActual();
-		std::cout << "(x:" << tiraActual->ObtenerX() <<", h:" << tiraActual->ObtenerAltura()<<") ";
+		std::cout << "(x:" << tiraActual->ObtenerX() << ", h:" << tiraActual->ObtenerAltura() << ") ";
 
 		iterTiras.Avanzar();
 	}
 	std::cout << endl;
+
+	// Algoritmo principal
+
 	return Array<Puntero<ITira>>(); //retorno por defecto
 };
 
--- Entrega5/main.cpp
+++ Entrega5/main.cpp
@@ -8,13 +8,37 @@ Puntero<ISistema> Inicializar()
 	return new Sistema();
 }
 
+void sil1()
+{
+	Puntero<ISistema> interfaz = Inicializar();
+	Array<Puntero<IEdificio>> ciudad(5);
+	ciudad[0] = new EdificioMock(2, 9, 10);
+	ciudad[1] = new EdificioMock(3, 7, 15);
+	ciudad[2] = new EdificioMock(5, 12, 12);
+	ciudad[3] = new EdificioMock(15, 20, 10);
+	ciudad[4] = new EdificioMock(19, 24, 8);
+
+	Array<Puntero<ITira>> siluetaEsperada(7);
+	siluetaEsperada[0] = new TiraMock(2, 10);
+	siluetaEsperada[1] = new TiraMock(3, 15);
+	siluetaEsperada[2] = new TiraMock(7, 12);
+	siluetaEsperada[3] = new TiraMock(12, 0);
+	siluetaEsperada[4] = new TiraMock(15, 10);
+	siluetaEsperada[5] = new TiraMock(20, 8);
+	siluetaEsperada[6] = new TiraMock(24, 0);
+
+	Array<Puntero<ITira>> siluetaObtenida = interfaz->CalcularSiluetaDeLaCiudad(ciudad);
+	
+}
 void main()
 {
-	Puntero<ConductorPrueba> cp = new ConductorPrueba();
+	/*Puntero<ConductorPrueba> cp = new ConductorPrueba();
 	Array<Puntero<Prueba>> pruebas = Array<Puntero<Prueba>>(3);
 	pruebas[0] = new PruebaMemoria();
 	pruebas[1] = new CasoDePrueba(Inicializar);
 	pruebas[2] = pruebas[0];
-	cp->CorrerPruebas(pruebas.ObtenerIterador());
+	cp->CorrerPruebas(pruebas.ObtenerIterador());*/
+
+	sil1();
 	system("pause");
 }
